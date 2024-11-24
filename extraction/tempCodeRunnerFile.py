fe54b28efed80c300e7887593bcd056048b2eb32
trongtin17 <tin.tranthanh@hcmut.edu.vn>
2024-11-24 10:32:54 +0700

    feat: add data extraction component

---
+++ extraction/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+ient_df, careplan_df, condition_df, diagnostic_report_df, encounter_df, immunization_df, observation_df, procedure_df = cleanAndRename(
+    #     patient_df, careplan_df, condition_df, diagnostic_report_df, encounter_df, immunization_df, observation_df, procedure_df
+    # )
